72a8655d2b2d6286806f00e8a2f40ea8bf36c0f8
Lars Costers <larscosters@gmail.com>
2018-11-12 11:02:28 +0100

    Cluster-based permutation testing for negative time-frequency clusters

--- osl_clustertf.m
+++ osl_clustertf.m
@@ -85,23 +85,30 @@ for i = 1:nP
         tg = reshape(tg,nF,nT);
     end;
     
-    [imlabel LL] = spm_bwlabel(double(tg>thresh),con);
-    tmp = unique(imlabel);
-    tmp(tmp==0) = [];
-    nL = 0;
-
-    if ~isempty(tmp)
-        for kk = 1:numel(tmp); %loop over clusters
-            k = tmp(kk);
-            if strcmp(permmeth,'clustextent')
-                nL(k) = sum(sum(imlabel==k));
-            elseif strcmp(permmeth,'clustmass')
-                nL(k) = sum(tg(imlabel==k));
+    for jj=1:2
+        if jj == 1
+            [imlabel LL] = spm_bwlabel(double(tg>thresh),con); %positive clusters
+        elseif jj == 2
+            [imlabel LL] = spm_bwlabel(double(tg<-thresh),con); %negative clusters
+        end
+        tmp = unique(imlabel);
+        tmp(tmp==0) = [];
+        nL = 0;
+        
+        if ~isempty(tmp)
+            for kk = 1:numel(tmp); %loop over clusters
+                k = tmp(kk);
+                if strcmp(permmeth,'clustextent')
+                    nL(k) = sum(sum(imlabel==k));
+                elseif strcmp(permmeth,'clustmass')
+                    nL(k) = sum(tg(imlabel==k));
+                end
+                tsmax(jj)=max(nL); %two-sided max
             end
-            nulldist(i)=max(nL);
         end
     end
-        
+    nulldist(i)=max(abs(tsmax));
+
 end
 
 %run a one sample t-test on data and compare cluster size to
@@ -126,20 +133,31 @@ else
     tg = reshape(tg,nF,nT);        
 end;
 
-[imlabel LL] = spm_bwlabel(double(tg>thresh),con);
-tmp = unique(imlabel);
-tmp(tmp==0) = [];
-cpimlabel = zeros(size(imlabel));
-if ~isempty(tmp)
-    for kk = 1:numel(tmp) %loop over clusters
-        k = tmp(kk);
-        if strcmp(permmeth,'clustextent')
-            nL = sum(sum(imlabel==k));
-        elseif strcmp(permmeth,'clustmass')
-            nL= sum(tg(imlabel==k));
+for ll=1:2
+    if ll == 1
+        [imlabel LL] = spm_bwlabel(double(tg>thresh),con); %positive clusters
+        cpimlabel = zeros(size(imlabel));
+    elseif jj == 2
+        [imlabel LL] = spm_bwlabel(double(tg<-thresh),con); %negative clusters
+    end
+    tmp = unique(imlabel);
+    sprintf('%d clusters found',size(tmp,1));
+    tmp(tmp==0) = [];
+    if ~isempty(tmp)
+        for kk = 1:numel(tmp) %loop over clusters
+            k = tmp(kk);
+            if strcmp(permmeth,'clustextent')
+                nL = abs(sum(sum(imlabel==k)));
+            elseif strcmp(permmeth,'clustmass')
+                nL= abs(sum(tg(imlabel==k)));
+            end
+            cp = mean(nL>nulldist);
+            if cp == 1 % if no cluster is larger
+                cp = 1-1/nP; % set p-value to 1/number of permutations
+            end
+            cpimlabel(imlabel==k)=cp;
+            sprintf('Cluster %d: value %.2f,  p = %.4f',kk,double(nL),(1-cp));
         end
-        cp = mean(nL>nulldist);
-      cpimlabel(imlabel==k)=cp;
     end
 end
 corrp = cpimlabel;
